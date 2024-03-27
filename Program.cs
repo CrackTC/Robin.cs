546e6f9972ca5d91ea8b2d9f8f650045e221676f
CrackTC <chenrke369@gmail.com>
2024-03-27 16:45:35 +0800

    feat: load assemblies

--- Program.cs
+++ Program.cs
@@ -15,13 +15,29 @@ builder.Services.AddHostedService<BotCreationService>()
 
 builder.Logging.AddConsole();
 
-var backends = AppDomain.CurrentDomain.GetAssemblies()
-    .SelectMany(assembly => assembly.GetExportedTypes())
-    .Select(type => (Type: type, Attributes: type.GetCustomAttributes<BackendAttribute>(false)))
-    .Where(pair => pair.Attributes.Any() && pair.Type.IsAssignableTo(typeof(IBackendFactory)))
-    .SelectMany(pair => pair.Attributes.Select(attribute => (attribute.Name, pair.Type)));
-
-foreach (var (name, type) in backends)
-    builder.Services.AddKeyedScoped(typeof(IBackendFactory), name, type);
+LoadAssemblies("Implementations");
+LoadAssemblies("Extensions");
+ConfigureBackend();
 
 await builder.Build().RunAsync();
+
+void ConfigureBackend()
+{
+    var backends = AppDomain.CurrentDomain.GetAssemblies()
+        .SelectMany(assembly => assembly.GetExportedTypes())
+        .Select(type => (Type: type, Attributes: type.GetCustomAttributes<BackendAttribute>(false)))
+        .Where(pair => pair.Attributes.Any() && pair.Type.IsAssignableTo(typeof(IBackendFactory)))
+        .SelectMany(pair => pair.Attributes.Select(attribute => (attribute.Name, pair.Type)));
+
+    foreach (var (name, type) in backends)
+        builder.Services.AddKeyedScoped(typeof(IBackendFactory), name, type);
+}
+
+void LoadAssemblies(string dir)
+{
+    var path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ?? string.Empty, dir);
+    foreach (var dll in Directory.GetFiles(path, "*.dll"))
+    {
+        Assembly.LoadFile(Path.GetFullPath(dll));
+    }
+}
