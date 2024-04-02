742713d4e1aac6fda8b8d6c0e803b34571ae053e
CrackTC <chenrke369@gmail.com>
2024-04-02 17:27:26 +0800

    style: some default param values

--- Robin.Abstractions/Message/Entities/Keyboard/KeyBoardPermission.cs
+++ Robin.Abstractions/Message/Entities/Keyboard/KeyBoardPermission.cs
@@ -1,3 +1,3 @@
 namespace Robin.Abstractions.Message.Entities.Keyboard;
 
-public record KeyboardPermission(KeyboardPermissionType Type, List<string>? RoleIds, List<string>? UserIds);
+public record KeyboardPermission(KeyboardPermissionType Type, List<string>? RoleIds = null, List<string>? UserIds = null);
