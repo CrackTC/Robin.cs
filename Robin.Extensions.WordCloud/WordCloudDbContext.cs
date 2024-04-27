80312a992dfcfc24cc97175d284c5ab6ea1e3803
CrackTC <chenrke369@gmail.com>
2024-04-27 19:28:48 +0800

    fix(extensions/word_cloud): db name

--- Robin.Extensions.WordCloud/WordCloudDbContext.cs
+++ Robin.Extensions.WordCloud/WordCloudDbContext.cs
@@ -2,13 +2,14 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Robin.Extensions.WordCloud;
 
+// ReSharper disable UnusedAutoPropertyAccessor.Global
 internal class WordCloudDbContext(long uin) : DbContext
 {
     public DbSet<Record> Records { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseSqlite($"Data Source=word_cloud-${uin}.db");
+        optionsBuilder.UseSqlite($"Data Source=word_cloud-{uin}.db");
     }
 
     protected override void OnModelCreating(ModelBuilder model)
