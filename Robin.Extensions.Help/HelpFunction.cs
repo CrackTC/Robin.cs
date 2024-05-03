42c30204bb2041213559fdd5a86e7cebf7861b9f
CrackTC <chenrke369@gmail.com>
2024-05-04 03:15:20 +0800

    style: remove unused usings

--- Robin.Extensions.Help/HelpFunction.cs
+++ Robin.Extensions.Help/HelpFunction.cs
@@ -3,7 +3,6 @@ using System.Text;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using Quartz;
 using Robin.Abstractions;
 using Robin.Abstractions.Communication;
 using Robin.Abstractions.Event;
