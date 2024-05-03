7d6639e22910866e42e2fe2fd24d27566a12c434
CrackTC <chenrke369@gmail.com>
2024-05-04 01:42:44 +0800

    feat(abstractions): requestext null handling

--- Robin.Abstractions/Operation/RequestExt.cs
+++ Robin.Abstractions/Operation/RequestExt.cs
@@ -4,6 +4,6 @@ namespace Robin.Abstractions.Operation;
 
 public static class RequestExt
 {
-    public static Task<Response?> SendAsync(this Request request, IOperationProvider operation, CancellationToken token = default)
-        => operation.SendRequestAsync(request, token);
+    public async static Task<Response?> SendAsync(this Request? request, IOperationProvider operation, CancellationToken token = default)
+        => request is not null ? await operation.SendRequestAsync(request, token) : new Response(false, -1, null);
 }
