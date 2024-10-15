04fbe0fa11f0e66b7ae5ab349909dbb5a106ce81
CrackTC <chenrke369@gmail.com>
2024-10-15 19:22:02 +0800

    feat: test text

--- Extensions/Robin.Extensions.Test/TestFunction.cs
+++ Extensions/Robin.Extensions.Test/TestFunction.cs
@@ -1,4 +1,4 @@
-﻿using Robin.Abstractions;
+using Robin.Abstractions;
 using Robin.Abstractions.Context;
 using Robin.Abstractions.Event.Message;
 using Robin.Abstractions.Message.Entity;
@@ -18,11 +18,11 @@ public class TestFunction(FunctionContext context) : BotFunction(context), IFlue
     public Task OnCreatingAsync(FunctionBuilder builder, CancellationToken _)
     {
         builder.On<GroupMessageEvent>()
-            .Where(ctx => ctx.Event.Message.Any(segment => segment is TextData { Text: "/test" }))
+            .Where(ctx => ctx.Event.Message.Any(segment => segment is TextData { Text: "/ping" }))
             .Select(ctx => (ctx.Event.GroupId, ctx.Token))
             .Do(ctx =>
                 new SendGroupMessageRequest(ctx.GroupId, [
-                    new TextData("Hello, world")
+                    new TextData("pong!")
                 ]).SendAsync(_context.OperationProvider, _context.Logger, ctx.Token)
             );
 
