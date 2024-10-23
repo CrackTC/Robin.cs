47ce79b64e24dda0583c1ae32920904a47e3ad0f
CrackTC <chenrke369@gmail.com>
2024-10-23 09:43:05 +0800

    chore(app): suppress log

--- Robin.App/Services/BotFunctionService.cs
+++ Robin.App/Services/BotFunctionService.cs
@@ -43,7 +43,6 @@ internal partial class BotFunctionService(
                 }
 
                 functions.Add(function);
-                LogAddingFunction(logger, info.Name);
 
                 foreach (var eventType in info.EventTypes)
                 {
@@ -141,9 +140,6 @@ internal partial class BotFunctionService(
 
     [LoggerMessage(EventId = 4, Level = LogLevel.Warning, Message = "Error while invoking function {Name}")]
     private static partial void LogInvokeFunctionFailed(ILogger logger, string name, Exception exception);
-
-    [LoggerMessage(EventId = 5, Level = LogLevel.Warning, Message = "Adding function {Name}")]
-    private static partial void LogAddingFunction(ILogger logger, string name);
 
     #endregion
 }
