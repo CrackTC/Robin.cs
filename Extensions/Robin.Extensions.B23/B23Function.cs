2e50b0efa243fc45dae545a1deba831631f13a8f
CrackTC <chenrke369@gmail.com>
2024-11-24 00:01:27 +0800

    chore(b23): add log

--- Extensions/Robin.Extensions.B23/B23Function.cs
+++ Extensions/Robin.Extensions.B23/B23Function.cs
@@ -1,5 +1,6 @@
 using System.Net;
 using System.Text.RegularExpressions;
+using Microsoft.Extensions.Logging;
 using Robin.Abstractions;
 using Robin.Abstractions.Context;
 using Robin.Abstractions.Event.Message;
@@ -27,10 +28,11 @@ public partial class B23Function(FunctionContext context) : BotFunction(context)
         using var resp = await _client.GetAsync(b23Url, token);
 
         if (resp is not { StatusCode: HttpStatusCode.Found, Headers.Location: { } location }) return null;
+        LogB23Redirect(_context.Logger, location.ToString());
         return RawUrlRegex.Match(location.ToString())?.Value;
     }
 
-    public Task OnCreatingAsync(FunctionBuilder builder, CancellationToken token)
+    public Task OnCreatingAsync(FunctionBuilder builder, CancellationToken _)
     {
         // cat card.json | jq .Message[0].Content --raw-output | jq .meta.detail_1.qqdocurl
         builder.On<MessageEvent>()
@@ -43,8 +45,11 @@ public partial class B23Function(FunctionContext context) : BotFunction(context)
                     ?? string.Empty;
 
                 if (!url.StartsWith("https://b23.tv/")) return;
+                LogExtractB23(_context.Logger, url);
+
                 if (await ResolveB23(url, ctx.Token) is not { } resolved) return;
 
+                LogB23Resolved(_context.Logger, resolved);
                 await ctx.Event.NewMessageRequest([
                     new ReplyData(ctx.Event.MessageId),
                     new TextData(resolved)
@@ -53,4 +58,17 @@ public partial class B23Function(FunctionContext context) : BotFunction(context)
 
         return Task.CompletedTask;
     }
+
+    #region Log
+
+    [LoggerMessage(Level = LogLevel.Information, Message = "Extract B23: {url}")]
+    private static partial void LogExtractB23(ILogger logger, string url);
+
+    [LoggerMessage(Level = LogLevel.Information, Message = "B23 redirect to {url}")]
+    private static partial void LogB23Redirect(ILogger logger, string url);
+
+    [LoggerMessage(Level = LogLevel.Information, Message = "B23 resolved to {url}")]
+    private static partial void LogB23Resolved(ILogger logger, string url);
+
+    #endregion
 }
