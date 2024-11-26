28609082e8983b516fb9cdfc6fb2dcb03ce33879
CrackTC <chenrke369@gmail.com>
2024-11-26 12:03:06 +0800

    chore(abs): log failed response

--- Robin.Abstractions/Operation/RequestExt.cs
+++ Robin.Abstractions/Operation/RequestExt.cs
@@ -19,26 +19,30 @@ public static partial class RequestExt
     {
         try
         {
-            if (await context.BotContext.OperationProvider.SendRequestAsync(request, token)
-                is not { Success: true } response)
-            {
-                LogSendFailed(context.Logger, request);
-                return null;
-            }
-
-            LogSent(context.Logger, request, response);
-            return response;
+            var resp = await context.BotContext.OperationProvider.SendRequestAsync(request, token);
+            if (resp is not { Success: true })
+                LogSendFailed(context.Logger, request, resp);
+            else
+                LogSent(context.Logger, request, resp);
+            return resp;
         }
         catch (Exception e)
         {
-            LogSendFailed(context.Logger, request, e);
+            LogSendException(context.Logger, request, e);
             return null;
         }
     }
 
+    #region Log
+
     [LoggerMessage(Level = LogLevel.Information, Message = "Sent request: {Request}, response: {Response}")]
     private static partial void LogSent(ILogger logger, Request request, Response response);
 
-    [LoggerMessage(Level = LogLevel.Warning, Message = "Failed to send request: {Request}")]
-    private static partial void LogSendFailed(ILogger logger, Request request, Exception? e = default);
+    [LoggerMessage(Level = LogLevel.Warning, Message = "Sending request failed: {Request}, response: {Response}")]
+    private static partial void LogSendFailed(ILogger logger, Request request, Response? response);
+
+    [LoggerMessage(Level = LogLevel.Warning, Message = "Exception occured when sending request: {Request}")]
+    private static partial void LogSendException(ILogger logger, Request request, Exception e);
+
+    #endregion
 }
