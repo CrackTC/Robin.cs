c33c224114136109ec9ba8f76c40c2a23e8647e3
CrackTC <chenrke369@gmail.com>
2024-12-01 23:25:05 +0800

    feat(onebot): add reaction event & operation

---
+++ Implementations/Robin.Implementations.OneBot/Entity/Events/Notice/OneBotReactionEvent.cs
@@ -0,0 +1,25 @@
+using System.Text.Json.Serialization;
+using Robin.Abstractions.Event;
+using Robin.Abstractions.Event.Notice.Reaction;
+using Robin.Implementations.OneBot.Converter;
+
+namespace Robin.Implementations.OneBot.Entity.Events.Notice;
+
+[Serializable]
+[OneBotEventType("reaction")]
+internal class OneBotReactionEvent : OneBotNoticeEvent
+{
+    [JsonPropertyName("sub_type")] public required string SubType { get; set; }
+    [JsonPropertyName("group_id")] public long GroupId { get; set; }
+    [JsonPropertyName("message_id")] public long MessageId { get; set; }
+    [JsonPropertyName("operator_id")] public long OperatorId { get; set; }
+    [JsonPropertyName("code")] public required string Code { get; set; }
+    [JsonPropertyName("count")] public required uint Count { get; set; }
+
+    public override BotEvent ToBotEvent(OneBotMessageConverter converter) =>
+        SubType switch
+        {
+            "add" => new ReactionAddEvent(Time, MessageId.ToString(), GroupId, OperatorId, Code, Count),
+            _ => new ReactionRemoveEvent(Time, MessageId.ToString(), GroupId, OperatorId, Code, Count)
+        };
+}
