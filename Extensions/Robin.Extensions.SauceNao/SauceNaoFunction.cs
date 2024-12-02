8af9a0faa0c27e23fca37a7e875aff4e8c203e4e
CrackTC <chenrke369@gmail.com>
2024-12-02 12:11:03 +0800

    feat(sauce): DoExpensive

--- Extensions/Robin.Extensions.SauceNao/SauceNaoFunction.cs
+++ Extensions/Robin.Extensions.SauceNao/SauceNaoFunction.cs
@@ -4,7 +4,6 @@ using Robin.Abstractions.Event.Message;
 using Robin.Abstractions.Message.Entity;
 using Robin.Abstractions.Operation;
 using Robin.Abstractions.Operation.Requests;
-using Robin.Abstractions.Operation.Responses;
 using Robin.Middlewares.Fluent;
 using Robin.Middlewares.Fluent.Event;
 using SauceNET;
@@ -18,48 +17,55 @@ public partial class SauceNaoFunction(
 {
     private SauceNETClient? _client;
 
+    private async Task<bool> HandleEvent(MessageEvent e, string msgId, CancellationToken token)
+    {
+        if (await new GetMessageRequest(msgId).SendAsync(_context, token)
+            is not { Message.Message: { } origMsg })
+            return false;
+
+        if (origMsg.OfType<ImageData>().FirstOrDefault() is not { Url: { } url })
+            return false;
+
+        var results = (await _client!.GetSauceAsync(url)).Results
+            .Where(result => double.TryParse(result.Similarity, out var s) && s >= 70.0)
+            .Take(3)
+            .Select(result =>
+                $"""
+                标题: {result.Name}
+                链接: {result.SourceURL}
+                相似度: {result.Similarity}%
+                来源: {result.DatabaseName}
+                """
+            )
+            .ToList();
+
+        if (results is [])
+        {
+            await e.NewMessageRequest([new TextData("找不到喵>_<")]).SendAsync(_context, token);
+            return false;
+        }
+
+        return await e.NewMessageRequest([
+            new TextData(string.Join("\n", results))
+        ]).SendAsync(_context, token) is { Success: true };
+    }
+
     public Task OnCreatingAsync(FunctionBuilder builder, CancellationToken _)
     {
         _client = new SauceNETClient(_context.Configuration.ApiKey);
 
-        builder.On<MessageEvent>()
+        builder.On<GroupMessageEvent>("group sauce")
             .OnCommand("搜图")
             .OnReply()
-            .Do(async t =>
-            {
-                var (ctx, msgId) = t;
-                var (e, token) = ctx;
-
-                if (await new GetMessageRequest(msgId).SendAsync(_context, token)
-                    is not { Message.Message: { } origMsg })
-                    return;
-
-                if (origMsg.OfType<ImageData>().FirstOrDefault() is not { Url: { } url })
-                    return;
-
-                var results = (await _client.GetSauceAsync(url)).Results
-                    .Where(result => double.TryParse(result.Similarity, out var s) && s >= 70.0)
-                    .Take(3)
-                    .Select(result =>
-                        $"""
-                        标题: {result.Name}
-                        链接: {result.SourceURL}
-                        相似度: {result.Similarity}%
-                        来源: {result.DatabaseName}
-                        """
-                    )
-                    .ToList();
-
-                if (results.Count is 0)
-                {
-                    await e.NewMessageRequest([new TextData("找不到喵>_<")]).SendAsync(_context, token);
-                    return;
-                }
-
-                await e.NewMessageRequest([
-                    new TextData(string.Join("\n", results))
-                ]).SendAsync(_context, token);
-            });
+            .DoExpensive(
+                t => HandleEvent(t.EventContext.Event, t.MessageId, t.EventContext.Token),
+                t => t.EventContext,
+                _context
+            )
+            .On<PrivateMessageEvent>("private sauce")
+            .OnCommand("搜图")
+            .OnReply()
+            .Do(t => HandleEvent(t.EventContext.Event, t.MessageId, t.EventContext.Token));
 
         return Task.CompletedTask;
     }
