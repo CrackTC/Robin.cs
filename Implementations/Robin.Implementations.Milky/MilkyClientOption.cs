b9dd1fdce4af765cb47a2e287708b1f514604e6a
CrackTC <chenrke369@gmail.com>
2026-01-10 23:12:15 +0800

    fix(impl/milky): Lagrange.Milky dirty workaround: add retries

--- Implementations/Robin.Implementations.Milky/MilkyClientOption.cs
+++ Implementations/Robin.Implementations.Milky/MilkyClientOption.cs
@@ -8,4 +8,5 @@ internal class MilkyClientOption
     public required string Url { get; set; }
     public int ReconnectInterval { get; set; }
     public string? AccessToken { get; set; }
+    public int SendMessageMaxRetry { get; set; } = 10;
 }
